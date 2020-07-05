7620bd5471364be093a7ef4cd28eede5ce868dc6
daniel-mizrahi <drm7wb@virginia.edu>
2020-07-05 13:50:57 -0400

    Problems

---
+++ Design/insert_delete_getrandom_380.py
@@ -0,0 +1,44 @@
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.set = set()
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.set:
+            return False
+        else:
+            self.set.add(val)
+            return True
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.set:
+            self.set.discard(val)
+            return True
+        return False
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.sample(list(self.set), 1)[0]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
